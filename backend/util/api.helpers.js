7c1c38addfa8ec2d32f4ee4914ad339c529acf0b
oriioll <oriolplazas2@gmail.com>
2026-05-19 20:38:19 +0200

    feat: add product validation and API helper utilities

---
+++ backend/util/api.helpers.js
@@ -0,0 +1,47 @@
+const getFeedback = (op) => {
+  return {
+    message: "Product" + op + "successfully",
+    status: "success",
+  };
+};
+
+const getError = (log, op, data) => {
+  return {
+    error: true,
+    message: `Cannot ${op} ${data}`,
+    log: log,
+    status: "error",
+    code: 500,
+  };
+};
+
+const validateProduct = (p) => {
+  if (
+    p.name === undefined ||
+    p.description === undefined ||
+    p.price === undefined ||
+    p.main_image === undefined ||
+    p.hover_image === undefined ||
+    p.about_image === undefined ||
+    p.info_image === undefined ||
+    p.weight === undefined
+  ) {
+    return false;
+  }
+  if (
+    typeof p.name !== "string" ||
+    typeof p.description !== "string" ||
+    typeof p.main_image !== "string" ||
+    typeof p.hover_image !== "string"
+  ) {
+    return false;
+  }
+  if (
+    typeof p.price !== "number" ||
+    p.price <= 0 ||
+    typeof p.weight !== "number"
+  ) {
+    return false;
+  }
+  return true;
+};
