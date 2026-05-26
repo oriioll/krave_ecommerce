9ab65b7a0cc4b2b52a9a1b65774692ec9c0d9897
oriioll <oriolplazas2@gmail.com>
2026-05-26 20:23:18 +0200

    feat: add cart repository functions

--- backend/repository/cartRepository.js
+++ backend/repository/cartRepository.js
@@ -7,3 +7,11 @@ export const getCartByUserId = async (user_id) => {
   );
   return response.rows ?? null;
 };
+
+export const getCartItemsByCartId = async (cart_id) => {
+  const response = await krave_ecommerce_db_pool.query(
+    "SELECT product_id, quantity FROM CART_ITEMS WHERE cart_id = $1",
+    [cart_id],
+  );
+  return response.rows ?? null;
+};
