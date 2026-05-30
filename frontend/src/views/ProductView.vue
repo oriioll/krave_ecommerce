12c37fed301ace0900a38d66f1c03bfced026dee
oriioll <oriolplazas2@gmail.com>
2026-05-31 01:16:38 +0200

    feat: update product view with add to cart functionality

--- frontend/src/views/ProductView.vue
+++ frontend/src/views/ProductView.vue
@@ -3,13 +3,21 @@ import ProductImages from '@/components/ProductImages.vue';
 import { getProductBySlug } from '@/services/products.fetcher.ts';
 import { onMounted, ref } from 'vue';
 import { useRoute } from "vue-router";
-import type { Product } from '@/types/Product';
+import type { Product } from '@/types/Product.ts';
 import Navbar from '@/components/Navbar.vue';
+import { postProductIntoCart } from '@/services/cart.fetcher.ts';
+import { userIsLogged } from '@/services/auth.fetcher.ts';
+import router from '@/router/router';
+import { useUIStore } from '@/stores/cartUi.store.ts'
+const cartUiStore = useUIStore()
 const product = ref<Product>()
 const route = useRoute();
 const slug = route.params.slug as string;
 const error = ref(false)
+const cartError = ref(false)
+const errorMsg = ref('')
 const isLoading = ref(false)
+const cartIsLoading = ref(false)
 onMounted(async () => {
     try {
         isLoading.value = true
@@ -26,6 +34,37 @@ onMounted(async () => {
         error.value = true;
     }
 })
+
+/**
+ * Add the product to the cart
+ * @param product_id The id of the product to add to the cart
+ * @author Oriol Plazas León
+ * @since 31/05/2026
+ */
+const addProductToCard = async (product_id: number) => {
+    try {
+        cartIsLoading.value = true;
+        errorMsg.value = ''
+        cartError.value = false;
+        //check if user is logged
+        const userData = await userIsLogged();
+        if (userData.error) {
+            //if user not logged, go to login
+            return router.push("/login")
+        }
+        errorMsg.value = '';
+        const data = await postProductIntoCart(product_id);
+        if (data.error) {
+            throw new Error('cannot add product to cart')
+        }
+        cartUiStore.openCart();
+        cartIsLoading.value = false
+    } catch (e: any) {
+        cartIsLoading.value = false;
+        cartError.value = true;
+        errorMsg.value = 'Cannot add product to card' + e.message;
+    }
+}
 </script>
 
 <template>
@@ -51,7 +90,25 @@ onMounted(async () => {
             <p>{{ product?.price }}€</p>
             <hr>
             <p>{{ product?.description }}</p>
-            <button class="addToCart">Add To Cart</button>
+            <button v-if="!cartIsLoading" @click="addProductToCard(product?.id!!)" class="addToCart">Add To
+                Cart</button>
+            <button v-else class="addToCart"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+                    viewBox="0 0 24 24">
+                    <g fill="none" stroke="var(--white)" stroke-linecap="round" stroke-linejoin="round"
+                        stroke-width="2">
+                        <path stroke-dasharray="18" d="M12 3c4.97 0 9 4.03 9 9">
+                            <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="18;0" />
+                            <animateTransform attributeName="transform" dur="1s" repeatCount="indefinite" type="rotate"
+                                values="0 12 12;360 12 12" />
+                        </path>
+                        <path stroke-dasharray="60"
+                            d="M12 3c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9c-4.97 0 -9 -4.03 -9 -9c0 -4.97 4.03 -9 9 -9Z"
+                            opacity=".3">
+                            <animate fill="freeze" attributeName="stroke-dashoffset" dur="1s" values="60;0" />
+                        </path>
+                    </g>
+                </svg></button>
+            <p class="errorMsg" v-if="cartError">{{ errorMsg }}</p>
         </article>
 
     </section>
