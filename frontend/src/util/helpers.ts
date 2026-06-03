b527f41f4e5e660bbcebd89b48c87d6217db4b47
oriioll <oriolplazas2@gmail.com>
2026-06-03 23:57:41 +0200

    feat: add link helper function

---
+++ frontend/src/util/helpers.ts
@@ -0,0 +1,9 @@
+/**
+ * Goes to the link
+ * @param link The link to open
+ * @author Oriol Plazas León
+ * @since 03/06/2026
+ */
+export const goToLink = (link: string) => {
+  window.location.href = link;
+};
