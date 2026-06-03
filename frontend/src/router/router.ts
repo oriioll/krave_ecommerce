888a20f582b7e0fe607a68a60cd2d2f3216e374a
oriioll <oriolplazas2@gmail.com>
2026-06-03 23:57:40 +0200

    feat: add product creation route

--- frontend/src/router/router.ts
+++ frontend/src/router/router.ts
@@ -5,6 +5,7 @@ import ProductView from "@/views/ProductView.vue";
 import LoginView from "@/views/LoginView.vue";
 import RegisterView from "@/views/RegisterView.vue";
 import ProfileView from "@/views/ProfileView.vue";
+import CreateProductView from "@/views/CreateProductView.vue";
 
 const router = createRouter({
   history: createWebHistory(),
@@ -33,6 +34,10 @@ const router = createRouter({
       path: "/profile",
       component: ProfileView,
     },
+    {
+      path: "/create-product",
+      component: CreateProductView,
+    },
   ],
   scrollBehavior(to, from, savedPosition) {
     if (savedPosition) {
@@ -48,7 +53,7 @@ router.beforeEach(async (to, _from, next) => {
   //check if userIsLogged to control authorization
   const authData = await userIsLogged();
   const guestAuthOnly = ["/login", "/register"];
-  const loggedOnly = ["/profile"];
+  const loggedOnly = ["/profile", "/create-product"];
   if (authData.loggedIn === true) {
     //if user is logged and tries to enter to login or register
     if (guestAuthOnly.includes(to.path)) {
