4a1cf0e66b3ddd6ef88283b4d8a3450314bca9b3
oriioll <oriolplazas2@gmail.com>
2026-06-04 00:45:23 +0200

    refactor: update auth repository

--- backend/repository/authRepository.js
+++ backend/repository/authRepository.js
@@ -9,13 +9,13 @@ import krave_ecommerce_db_pool from "../db/setupdb.js";
  * @author Oriol Plazas León
  * @throws Error if insert fails
  */
-export const createUser = async (email, pwd, name) => {
+export const createCustomer = async (email, pwd, name) => {
   const response = await krave_ecommerce_db_pool.query(
     `INSERT INTO users 
-    (email, password, name)
-   VALUES ($1, $2, $3)
+    (email, password, name, role_id)
+   VALUES ($1, $2, $3, $4)
    RETURNING *`,
-    [email, pwd, name],
+    [email, pwd, name, 2],
   );
   return response.rows[0];
 };
