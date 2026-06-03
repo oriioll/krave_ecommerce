f2dd769a14fd628253e0f010e77d342faec70e3b
oriioll <oriolplazas2@gmail.com>
2026-06-04 00:45:23 +0200

    feat: add isAdminOrStaff middleware

--- backend/middleware/role.js
+++ backend/middleware/role.js
@@ -1,9 +1,23 @@
+import { extractUserFromToken } from "../util/api.helpers.js";
+
 /**
  * Middleware function that checks if the authenticathed user is an admin or not
  */
 export const isAdmin = async (req, res, next) => {
   const { role } = extractUserFromToken(req);
   if (role != "admin") {
+    return res
+      .status(403)
+      .json({ error: true, status: "error", message: "Forbidden" });
+  } else {
+    next();
+  }
+};
+
+export const isAdminOrStaff = async (req, res, next) => {
+  const { role } = extractUserFromToken(req);
+  console.log(role);
+  if (role != "admin" && role != "staff") {
     return res
       .status(403)
       .json({ error: true, status: "error", message: "Forbidden" });
