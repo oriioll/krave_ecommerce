1162a3b21369b5dcb510ba95f7b9b31a35c1e0c4
oriioll <oriolplazas2@gmail.com>
2026-06-06 23:58:32 +0200

    feat: add new view for rendering products to edit

---
+++ frontend/src/views/RenderProductsToEditView.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import Navbar from '@/components/Navbar.vue';
+import ProductsMain from '@/components/ProductsMain.vue';
+const EDIT_MODE: boolean = true
+</script>
+
+<template>
+    <Navbar class="nav" />
+    <ProductsMain :editMode="EDIT_MODE" />
+</template>
+
+<style scoped></style>
