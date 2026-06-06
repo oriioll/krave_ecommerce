95ef1218d94fff326e32bfbacf03b8cc5aa3014a
oriioll <oriolplazas2@gmail.com>
2026-06-07 01:44:05 +0200

    refactor: change image handling logic in ProductImages.vue

--- frontend/src/components/ProductImages.vue
+++ frontend/src/components/ProductImages.vue
@@ -1,20 +1,19 @@
 <script setup lang="ts">
-import { ref } from 'vue';
+import { computed } from 'vue';
 import type { Product } from '@/types/Product';
 const props = defineProps<{
     product: Product
 }>()
 
 //Get an image array filtered to only get not null images
-const images = ref<string[]>([])
-images.value = [
-    props.product.main_image,
-    props.product.hover_image,
-    props.product.about_image,
-    props.product.info_image
-].filter(i => i != null && i != undefined);
-
-
+const images = computed(() => {
+    return [
+        props.product.main_image,
+        props.product.hover_image,
+        props.product.about_image,
+        props.product.info_image
+    ].filter(i => i != null && i != undefined && i != '');
+})
 const goToImage = (dir: string) => {
     const hash = window.location.hash;
     const currentImg = window.location.hash.replace('#image-', '');
@@ -44,7 +43,7 @@ const goToImage = (dir: string) => {
                     <path fill="var(--black)"
                         d="M11.8 13H15q.425 0 .713-.288T16 12t-.288-.712T15 11h-3.2l.9-.9q.275-.275.275-.7t-.275-.7t-.7-.275t-.7.275l-2.6 2.6q-.3.3-.3.7t.3.7l2.6 2.6q.275.275.7.275t.7-.275t.275-.7t-.275-.7zm.2 9q-2.075 0-3.9-.788t-3.175-2.137T2.788 15.9T2 12t.788-3.9t2.137-3.175T8.1 2.788T12 2t3.9.788t3.175 2.137T21.213 8.1T22 12t-.788 3.9t-2.137 3.175t-3.175 2.138T12 22" />
                 </svg>
-                <img v-for="(image, index) in images" :src="image" alt="Image of the product" :id="'image-' + index">
+                <img v-for="(image, index) in images" :src="image!" alt="Image of the product" :id="'image-' + index">
                 <svg @click="() => goToImage('right')" xmlns="http://www.w3.org/2000/svg" width="40" height="40"
                     viewBox="0 0 24 24" class="arrow right">
                     <path fill="var(--black)"
