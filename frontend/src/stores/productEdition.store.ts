6149d013279cae15c2469fc941744ac2485341cc
oriioll <oriolplazas2@gmail.com>
2026-06-09 22:58:53 +0200

    feat: add product modification functionality

--- frontend/src/stores/productEdition.store.ts
+++ frontend/src/stores/productEdition.store.ts
@@ -1,5 +1,7 @@
 import router from "@/router/router";
-import { deleteProductById } from "@/services/products.fetcher";
+import { deleteProductById, putProductById } from "@/services/products.fetcher";
+import type { Product } from "@/types/Product";
+import { handleImagesNulls, validateProduct } from "@/util/validators";
 import { defineStore } from "pinia";
 import { ref } from "vue";
 export const useProductManagementStore = defineStore("editProducts", () => {
@@ -7,6 +9,38 @@ export const useProductManagementStore = defineStore("editProducts", () => {
   const isLoadingDelete = ref(false);
   const error = ref(false);
   const errorMsg = ref("");
+  const handleProductModification = async (product: Product) => {
+    try {
+      error.value = false;
+      errorMsg.value = "";
+      isLoadingEdit.value = true;
+      //Validate product fields
+      const validation = validateProduct(product);
+      if (!validation.valid) {
+        error.value = true;
+        errorMsg.value = validation.error;
+        isLoadingEdit.value = false;
+        return;
+      }
+      //transform empty string to null
+      const transformedProduct = handleImagesNulls(product);
+      const data = await putProductById(product.id!, transformedProduct);
+      if (data.error) {
+        //If api post fails
+        error.value = true;
+        errorMsg.value =
+          "Cannot edit product. - Make sure all fields are filled ";
+        isLoadingEdit.value = false;
+        return;
+      }
+      //if post is successfull
+      router.push("/manage-products");
+    } catch (e: any) {
+      error.value = true;
+      errorMsg.value = "Cannot edit product";
+      isLoadingEdit.value = false;
+    }
+  };
   const handleProductDelete = async (product_id: number) => {
     try {
       error.value = false;
@@ -34,5 +68,6 @@ export const useProductManagementStore = defineStore("editProducts", () => {
     error,
     errorMsg,
     handleProductDelete,
+    handleProductModification,
   };
 });
