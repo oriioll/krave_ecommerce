f3b470a687f77004b80cf8d79e58122ced41541c
oriioll <oriolplazas2@gmail.com>
2026-06-17 21:17:07 +0200

    feat: create manage users view

---
+++ frontend/src/views/ManageUsersView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import OnlyLogoNavbar from '@/components/OnlyLogoNavbar.vue';
+import UsersList from '@/components/UsersList.vue';
+
+</script>
+
+<template>
+    <OnlyLogoNavbar />
+    <main>
+        <UsersList />
+    </main>
+</template>
+
+<style scoped>
+@media (min-width: 800px) {}
+</style>
