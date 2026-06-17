73987088e6d778941ef62c8002f30ee6cfe168aa
oriioll <oriolplazas2@gmail.com>
2026-06-18 00:38:31 +0200

    feat: add user deletion functionality

--- frontend/src/components/UsersList.vue
+++ frontend/src/components/UsersList.vue
@@ -1,13 +1,14 @@
 <script setup lang="ts">
 
-import { getUsers } from '@/services/users.fetcher';
+import { deleteUserById, getUsers } from '@/services/users.fetcher';
 import type { User } from '@/types/User';
 import { ref, onMounted } from 'vue';
 import UsersFilters from './UsersFilters.vue';
 const error = ref<boolean>(false)
+const errorMsg = ref('')
 const users = ref<User[]>([]);
 const isLoading = ref(true)
-
+const opsError = ref(false)
 onMounted(async () => {
     try {
         isLoading.value = true;
@@ -22,6 +23,11 @@ onMounted(async () => {
 
 })
 
+/**
+ * Orders the reactive array of users based on criteria and order
+ * @param criteria The criteria of the order (name, mail or role)
+ * @param asc if want to order ascendant or descendant
+ */
 const handleOrderBy = (criteria: string, asc: boolean) => {
     if (criteria === 'name') {
         if (asc) {
@@ -43,12 +49,32 @@ const handleOrderBy = (criteria: string, asc: boolean) => {
         }
     }
 }
+const deleteLoading = ref(false)
+const deleteUser = async (user: User) => {
+    try {
+        opsError.value = false;
+        const success = await deleteUserById(user.id!)
+        if (success.error) {
+            throw new Error('Cannot delete user API RETURNED ERROR: ' + success.message + success.log)
+        }
+        //Reset the users list
+        const data = await getUsers();
+        users.value = data;
+    } catch (e: any) {
+        opsError.value = true;
+        errorMsg.value = 'Cannot delete user ' + user.name
+    }
+}
 </script>
 
 <template>
     <div class="mainUsers">
-        <h2>Manage the users</h2>
+        <article class="title">
+            <h2>Users</h2>
+            <h4>+ Add User</h4>
+        </article>
         <UsersFilters @change-order="handleOrderBy" />
+        <span class="errorMsg" v-if="opsError">{{ errorMsg }}</span>
         <section v-if=isLoading class="usersList loading">
             <div v-for="_ in 5" class="skeleton-row">
                 <div class="skeleton-content">
@@ -69,7 +95,23 @@ const handleOrderBy = (criteria: string, asc: boolean) => {
 
                 <div class="col-actions">
                     <button class="btn-edit">Edit</button>
-                    <button class="deleteUser">Delete User</button>
+                    <button v-if="!deleteLoading" @click="deleteUser(user)" class="deleteUser">Delete User</button>
+                    <button v-else class="deleteUser"><svg xmlns="http://www.w3.org/2000/svg" width="30" height="30"
+                            viewBox="0 0 24 24">
+                            <g fill="none" stroke="var(--white)" stroke-linecap="round" stroke-linejoin="round"
+                                stroke-width="2">
+                                <path stroke-dasharray="18" d="M12 3c4.97 0 9 4.03 9 9">
+                                    <animate fill="freeze" attributeName="stroke-dashoffset" dur="0.3s" values="18;0" />
+                                    <animateTransform attributeName="transform" dur="1s" repeatCount="indefinite"
+                                        type="rotate" values="0 12 12;360 12 12" />
+                                </path>
+                                <path stroke-dasharray="60"
+                                    d="M12 3c4.97 0 9 4.03 9 9c0 4.97 -4.03 9 -9 9c-4.97 0 -9 -4.03 -9 -9c0 -4.97 4.03 -9 9 -9Z"
+                                    opacity=".3">
+                                    <animate fill="freeze" attributeName="stroke-dashoffset" dur="1s" values="60;0" />
+                                </path>
+                            </g>
+                        </svg></button>
                 </div>
             </article>
         </section>
@@ -82,6 +124,12 @@ const handleOrderBy = (criteria: string, asc: boolean) => {
 <style scoped>
 p {
     margin: 0;
+}
+
+.title {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
 }
 
 .mainUsers {
@@ -168,9 +216,9 @@ p {
 }
 
 .usersList button {
-    padding: .5rem .5rem;
+    padding: .5rem 1.5rem;
     font-weight: 600;
-    font-size: var(--step-0);
+    font-size: var(--step--1);
 }
 
 .deleteUser {
@@ -184,6 +232,12 @@ p {
 }
 
 @media (min-width: 1000px) {
+    .usersList button {
+        padding: .5rem .5rem;
+        font-weight: 600;
+        font-size: var(--step-0);
+    }
+
     .mainUsers {
         padding: 2rem;
     }
